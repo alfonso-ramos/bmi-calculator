73813348bfb62ba5f72e90bb5d0b532cd7b11f2b
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-03-21 09:09:56 -0700

    [ADD] Agregada la logica del componente que calcula el BMI, falta estilizar

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,61 +1,95 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue';
 
-const system = ref('metric')
-let height = ref(0)
-let weight = ref(0)
-let heightFt = ref(0)
-let heightIn = ref(0)
-let weightSt = ref(0)
-let weightLbs = ref(0)
+const selectedSystem = ref('metric');
+const height = ref(null);
+const weight = ref(null);
+const feet = ref(null);
+const inches = ref(null);
+const stones = ref(null);
+const pounds = ref(null);
+const bmi = ref(null);
+const isCalculating = ref(false); 
 
-const bmi = ref(0)
 const calculateBMI = () => {
-    if (system === 'metric') {
-        return (weight / height**2 )
-    } else {
-        // Realizar calculo para el bmi en sistema ingles
-    }
-}
-const handleChange = (e) => {
-    height = 0
-    weight = 0
-    heightFt = 0
-    heightIn = 0
-    weightSt = 0
-    weightLbs = 0
-}
+    isCalculating.value = true; 
+    setTimeout(() => {
+        if (selectedSystem.value === 'metric') {
+            const heightInMeters = height.value / 100;
+            bmi.value = weight.value / (heightInMeters * heightInMeters);
+        } else {
+            const heightInInches = feet.value * 12 + inches.value;
+            const weightInPounds = stones.value * 14 + pounds.value;
+            bmi.value = (weightInPounds * 703) / (heightInInches * heightInInches);
+            
+        }
+
+        isCalculating.value = false;
+    }, 1000);
+};
+
+watch([height, weight, feet, inches, stones, pounds], () => {
+    calculateBMI();
+});
+
 </script>
 
 <template>
-    <div class="bg-white rounded-2xl flex flex-col p-6">
-        <h2 class="text-2xl text-gunmetal">Enter your details below</h2>
-        <div class="flex items-center cursor-pointer">
-            <form @submit.prevent="handleChange"></form>
-            <input 
-                class="cursor-pointer w-[31px] h-[31px]"
-                type="radio" 
-                id="metric" 
-                name="system" 
-                value="metric" 
-                checked="{{ }}" 
-                @click="handleChange"
-            >
-            <label class="ml-4 font-semibold cursor-pointer text-gunmetal" for="metric">Metric</label>
+    <div>
+        <h2>Enter yout details below</h2>
+        <div>
+            <label>
+                <input type="radio" v-model="selectedSystem" value="metric">
+                Metric
+            </label>
+            <label>
+                <input type="radio" v-model="selectedSystem" value="imperial">
+                Imperial
+            </label>
         </div>
-        <div class="flex items-center cursor-pointer">
-            <form @submit.prevent="handleChange"></form>
-            <input 
-                class="cursor-pointer w-[31px] h-[31px]"
-                type="radio" 
-                id="imperial" 
-                name="system" 
-                value="metric" 
-                checked="{{ }}" 
-                @click="handleChange"
-            >
-            <label class="ml-4 font-semibold cursor-pointer text-gunmetal" for="imperial">Imperial</label>
+
+        <div v-if="selectedSystem === 'metric'">
+            <label for="height">height</label>
+            <div class="flex bg-white rounded-lg border border-solid border-borders">
+                <input id="height" type="number" inputmode="numeric" min="0" placeholder="0" v-model.number="height">
+                <p>cm</p>
+            </div>
+            <label for="weight">Weight</label>
+            <div class="flex bg-white rounded-lg border border-solid border-borders">
+                <input id="weight" placeholder="0" type="number" v-model.number="weight">
+                <p>kg</p>
+            </div>
         </div>
+        <div v-else>
+            <label for="feet">Height</label>
+            <div>
+                <div>
+                    <input id="feet" type="number" v-model.number="feet">
+                    <p>ft</p>
+                </div>
+                <div>
+                    <input id="inches" type="number" v-model.number="inches">
+                    <p>in</p>
+                </div>
+            </div>
+            <label for="stones">Weight</label>
+            <div>   
+                <div>
+                    <input id="stones" type="number" v-model.number="stones">
+                    <p>st</p>
+                </div>
+                <div>
+                    <input id="pounds" type="number" v-model.number="pounds">
+                    <p>lbs</p>
+                </div>
+            </div>
+        </div>
+        <div v-if="isCalculating">
+            <p>Calculating...</p>
+        </div>
+        <div v-else>
+            <p v-if="bmi !== null">BMI : {{ bmi.toFixed(1) }}</p>
+        </div>
+
     </div>
 </template>
-
